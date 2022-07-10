29773a22c75c74b58345cd3ec3d2500da44c9f03
OKuzmanov <okuzmanov@outlook.com>
2022-07-10 12:55:27 +0300

    Updated Kanbanize API Tests

--- Kanbanize_API_Tests/Kanbanize.Api.Tests/Cards.cs
+++ Kanbanize_API_Tests/Kanbanize.Api.Tests/Cards.cs
@@ -68,6 +68,61 @@ namespace Kanbanize.Api.Tests
 
         [XmlElement(ElementName = "color")]
         public string color { get; set; }
+
+        [XmlElement(ElementName = "subtaskdetails")]
+        public SubtaskDetailItems subtaskdetails { get; set; }
+
+        [XmlElement(ElementName = "comments")]
+        public CommentItems commentItems { get; set; }
+    }
+
+    [XmlRoot(ElementName = "comments")]
+    public class CommentItems
+    {
+        [XmlElement(ElementName ="item")]
+        public List<CommentDetails> comments { get; set;}
+    }
+
+    [XmlRoot(ElementName ="item")]
+    public class CommentDetails
+    {
+        [XmlElement(ElementName = "commentid")]
+        public long commentId { get; set; }
+
+        [XmlElement(ElementName = "author")]
+        public string author { get; set; }
+
+        [XmlElement(ElementName = "event")]
+        public string eventName { get; set; }
+
+        [XmlElement(ElementName = "text")]
+        public string text { get; set; }
+    }
+
+    [XmlRoot(ElementName ="subtaskdetails")]
+    public class SubtaskDetailItems
+    {
+        [XmlElement(ElementName = "item")]
+        public List<SubtaskDetail> subtasks { get; set; }
+    }
+
+    public class SubtaskDetail
+    {
+        [XmlElement(ElementName = "taskid")]
+        public long id { get; set; }
+
+        [XmlElement(ElementName = "title")]
+        public string title { get; set; }
+    }
+
+    [XmlRoot(ElementName = "xml")]
+    public class RootXmlAddComment
+    {
+        [XmlElement(ElementName = "commentid")]
+        public long commentId { get; set; }
+
+        [XmlElement(ElementName = "commentText")]
+        public string commentText { get; set; }
     }
 
     [XmlRoot(ElementName = "xml")]
@@ -82,5 +137,12 @@ namespace Kanbanize.Api.Tests
     {
         [XmlElement(ElementName = "item")]
         public List<Details> cards { get; set; }
+    }
+
+    [XmlRoot(ElementName = "xml")]
+    public class RootXmlAddSubtask
+    {
+        [XmlElement(ElementName = "item")]
+        public long id { get; set; }
     }
 }
--- Kanbanize_API_Tests/Kanbanize.Api.Tests/CardsAPITests.cs
+++ Kanbanize_API_Tests/Kanbanize.Api.Tests/CardsAPITests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using RestSharp;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Xml.Serialization;
@@ -364,7 +365,334 @@ namespace Kanbanize.Api.Tests
             Assert.That(countCardsToDoColumnAfter == countCardsToDoColumnBefore - 1);
         }
 
-        private RootXmlAllCardsFromBoard GetAllTasksFromBoard(long boardId, params string[] columns)
+        [Test]
+        public void Test_AddSubtask_ValidData()
+        {
+            //Create new card in column To Do.
+            const string newTitle = "Test Task Edit";
+            const string newDescription = "Test task to be edited via RestSharp";
+            const string newPriority = "Average";
+            const string newColor = "#f37325";
+            const string newDate = "2022-12-01";
+            const string newColumn = "To Do";
+
+            //Get the board id of the first board in the first project.
+            long boardId = this.GetAllProjectsAndBoards().projects.projectItems[0].boards.boardItems[0].id;
+
+            RestRequest requestCreateCard = new RestRequest(apiUrl + "/create_new_task/", Method.Post);
+            requestCreateCard.AddHeader("apikey", apiKey);
+            requestCreateCard.AddBody(new
+            {
+                boardid = boardId,
+                title = newTitle,
+                description = newDescription,
+                priority = newPriority,
+                color = newColor,
+                deadline = newDate,
+                column = newColumn,
+                returntaskdetails = "1"
+            });
+
+            RestResponse responseCreateCard = this.client.Execute(requestCreateCard);
+
+            Assert.AreEqual(HttpStatusCode.OK, responseCreateCard.StatusCode);
+            StringReader strReaderCreateCard = new StringReader(responseCreateCard.Content);
+
+            XmlSerializer xmlSerializerCreateCard = new XmlSerializer(typeof(RootXmlCreateCards));
+            RootXmlCreateCards? rootXmlCreateCards = (RootXmlCreateCards)xmlSerializerCreateCard.Deserialize(strReaderCreateCard);
+
+            long newlyCreatedCardID = rootXmlCreateCards.id;
+
+            //Add a new Subtask in the newly created Card
+            const string subtaskTitle = "Sample subtask title";
+
+            RestRequest requestAddSubtask = new RestRequest(apiUrl + "/add_subtask/", Method.Post);
+            requestAddSubtask.AddHeader("apikey", apiKey);
+            requestAddSubtask.AddBody(new {
+                taskparent = newlyCreatedCardID,
+                title = subtaskTitle
+            });
+
+            RestResponse responseAddSubtask = this.client.Execute(requestAddSubtask);
+
+            Assert.AreEqual(HttpStatusCode.OK, responseAddSubtask.StatusCode);
+
+            StringReader strReader = new StringReader(responseAddSubtask.Content);
+
+            XmlSerializer? xmlSerializerAddSubtask = new XmlSerializer(typeof(RootXmlAddSubtask));
+            RootXmlAddSubtask? rootXmlAddSubtask = (RootXmlAddSubtask)xmlSerializerAddSubtask.Deserialize(strReader);
+
+            long newlyCreatedSubtaskID = rootXmlAddSubtask.id;
+
+            RootXmlGetTaskDetails? rootXmlGetTaskDetails = this.GetTaskDetails(boardId, newlyCreatedCardID);
+
+            List<SubtaskDetail> subtasks = rootXmlGetTaskDetails.subtaskdetails.subtasks;
+
+            bool isSubTaskCreated = false;
+
+            foreach (SubtaskDetail subtask in subtasks)
+            {
+                if (subtask.id == newlyCreatedSubtaskID)
+                {
+                    Assert.AreEqual(subtaskTitle, subtask.title);
+                    isSubTaskCreated = true;
+                }
+            }
+
+            if (!isSubTaskCreated)
+            {
+                Assert.Fail("Subtask is not created properly!");
+            }
+        }
+
+        [Test]
+        public void Test_AddSubtask_InvalidData()
+        {
+            const string expectedErrMsg = "The taskparent parameter was not set correctly.";
+            const string subtaskTitle = "Sample subtask title";
+
+            RestRequest requestAddSubtask = new RestRequest(apiUrl + "/add_subtask/", Method.Post);
+            requestAddSubtask.AddHeader("apikey", apiKey);
+            requestAddSubtask.AddBody(new
+            {
+                title = subtaskTitle
+            });
+
+            RestResponse responseAddSubtask = this.client.Execute(requestAddSubtask);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, responseAddSubtask.StatusCode);
+
+            StringReader strReader = new StringReader(responseAddSubtask.Content);
+
+            XmlSerializer? xmlSerializerAddSubtask = new XmlSerializer(typeof(RootXmlInvalidCardInput));
+            RootXmlInvalidCardInput? rootXmlInvalidCardInput = (RootXmlInvalidCardInput)xmlSerializerAddSubtask.Deserialize(strReader);
+
+            Assert.AreEqual(expectedErrMsg, rootXmlInvalidCardInput.errMsg);
+        }
+
+        [Test]
+        public void Test_EdditSubtask_ValidData()
+        {
+            //Create new card in column To Do.
+            const string newTitle = "Test Task Edit";
+            const string newDescription = "Test task to be edited via RestSharp";
+            const string newPriority = "Average";
+            const string newColor = "#f37325";
+            const string newDate = "2022-12-01";
+            const string newColumn = "To Do";
+
+            //Get the board id of the first board in the first project.
+            long boardId = this.GetAllProjectsAndBoards().projects.projectItems[0].boards.boardItems[0].id;
+
+            RestRequest requestCreateCard = new RestRequest(apiUrl + "/create_new_task/", Method.Post);
+            requestCreateCard.AddHeader("apikey", apiKey);
+            requestCreateCard.AddBody(new
+            {
+                boardid = boardId,
+                title = newTitle,
+                description = newDescription,
+                priority = newPriority,
+                color = newColor,
+                deadline = newDate,
+                column = newColumn,
+                returntaskdetails = "1"
+            });
+
+            RestResponse responseCreateCard = this.client.Execute(requestCreateCard);
+
+            Assert.AreEqual(HttpStatusCode.OK, responseCreateCard.StatusCode);
+            StringReader strReaderCreateCard = new StringReader(responseCreateCard.Content);
+
+            XmlSerializer xmlSerializerCreateCard = new XmlSerializer(typeof(RootXmlCreateCards));
+            RootXmlCreateCards? rootXmlCreateCards = (RootXmlCreateCards)xmlSerializerCreateCard.Deserialize(strReaderCreateCard);
+
+            long newlyCreatedCardID = rootXmlCreateCards.id;
+
+            //Add a new Subtask in the newly created Card
+            const string subtaskTitle = "Sample subtask title";
+
+            RestRequest requestAddSubtask = new RestRequest(apiUrl + "/add_subtask/", Method.Post);
+            requestAddSubtask.AddHeader("apikey", apiKey);
+            requestAddSubtask.AddBody(new
+            {
+                taskparent = newlyCreatedCardID,
+                title = subtaskTitle
+            });
+
+            RestResponse responseAddSubtask = this.client.Execute(requestAddSubtask);
+
+            Assert.AreEqual(HttpStatusCode.OK, responseAddSubtask.StatusCode);
+
+            StringReader strReader = new StringReader(responseAddSubtask.Content);
+
+            XmlSerializer? xmlSerializerAddSubtask = new XmlSerializer(typeof(RootXmlAddSubtask));
+            RootXmlAddSubtask? rootXmlAddSubtask = (RootXmlAddSubtask)xmlSerializerAddSubtask.Deserialize(strReader);
+
+            long newlyCreatedSubtaskID = rootXmlAddSubtask.id;
+
+            //Edit newly created Subtask
+            const string editedTitle = "Edited subtask title";
+            const string completeStatus = "1";
+
+            RestRequest requestEditSubtask = new RestRequest(apiUrl + "/edit_subtask/", Method.Post);
+            requestEditSubtask.AddHeader("apikey", apiKey);
+            requestEditSubtask.AddBody(new
+            {
+                boardid = boardId,
+                subtaskid = newlyCreatedSubtaskID,
+                title = editedTitle,
+                complete = completeStatus
+            });
+
+            RestResponse responseEditTask = this.client.Execute(requestEditSubtask);
+
+            Assert.AreEqual(HttpStatusCode.OK, responseEditTask.StatusCode);
+
+            //Assert subtask is edited
+            RootXmlGetTaskDetails? rootXmlGetTaskDetails = this.GetTaskDetails(boardId, newlyCreatedCardID);
+
+            List<SubtaskDetail> subtasks = rootXmlGetTaskDetails.subtaskdetails.subtasks;
+
+            foreach (SubtaskDetail subtask in subtasks)
+            {
+                if (subtask.id == newlyCreatedSubtaskID)
+                {
+                    Assert.AreEqual(editedTitle, subtask.title);
+                }
+            }
+        }
+
+
+        [Test]
+        public void Test_EdditSubtask_InvalidData()
+        {
+            const string expectedErrMsg = "The boardid parameter was not set correctly.";
+            const string editedTitle = "Edited subtask title";
+            const string completeStatus = "1";
+
+            RestRequest requestEditSubtask = new RestRequest(apiUrl + "/edit_subtask/", Method.Post);
+            requestEditSubtask.AddHeader("apikey", apiKey);
+            requestEditSubtask.AddBody(new
+            {
+                title = editedTitle,
+                complete = completeStatus
+            });
+
+            RestResponse responseEditTask = this.client.Execute(requestEditSubtask);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, responseEditTask.StatusCode);
+
+            StringReader strReader = new StringReader(responseEditTask.Content);
+
+            XmlSerializer xmlSerializerErrMsg = new XmlSerializer(typeof(RootXmlInvalidCardInput));
+            RootXmlInvalidCardInput? rootXmlInvalidCardInput = (RootXmlInvalidCardInput)xmlSerializerErrMsg.Deserialize(strReader);
+
+            Assert.AreEqual(expectedErrMsg, rootXmlInvalidCardInput.errMsg);
+        }
+
+        [Test]
+        public void Test_AddComment_ValidData()
+        {
+            //Create new card in column To Do.
+            const string newTitle = "Test Task Edit";
+            const string newDescription = "Test task to be edited via RestSharp";
+            const string newPriority = "Average";
+            const string newColor = "#f37325";
+            const string newDate = "2022-12-01";
+            const string newColumn = "To Do";
+
+            //Get the board id of the first board in the first project.
+            long boardId = this.GetAllProjectsAndBoards().projects.projectItems[0].boards.boardItems[0].id;
+
+            RestRequest requestCreateCard = new RestRequest(apiUrl + "/create_new_task/", Method.Post);
+            requestCreateCard.AddHeader("apikey", apiKey);
+            requestCreateCard.AddBody(new
+            {
+                boardid = boardId,
+                title = newTitle,
+                description = newDescription,
+                priority = newPriority,
+                color = newColor,
+                deadline = newDate,
+                column = newColumn,
+                returntaskdetails = "1"
+            });
+
+            RestResponse responseCreateCard = this.client.Execute(requestCreateCard);
+
+            Assert.AreEqual(HttpStatusCode.OK, responseCreateCard.StatusCode);
+            StringReader strReaderCreateCard = new StringReader(responseCreateCard.Content);
+
+            XmlSerializer xmlSerializerCreateCard = new XmlSerializer(typeof(RootXmlCreateCards));
+            RootXmlCreateCards? rootXmlCreateCards = (RootXmlCreateCards)xmlSerializerCreateCard.Deserialize(strReaderCreateCard);
+
+            long newlyCreatedCardID = rootXmlCreateCards.id;
+
+            //Add a comment in the newly created Card
+            const string newComment = "This is a new test comment";
+
+            RestRequest requestAddComment = new RestRequest(apiUrl + "/add_comment/", Method.Post);
+            requestAddComment.AddHeader("apikey", apiKey);
+            requestAddComment.AddBody(new
+            {
+                taskid = newlyCreatedCardID,
+                comment = newComment
+            });
+
+            RestResponse responseAddComment = this.client.Execute(requestAddComment);
+
+            Assert.AreEqual(HttpStatusCode.OK, responseAddComment.StatusCode);
+
+            StringReader strReaderAddComment = new StringReader(responseAddComment.Content);
+
+            XmlSerializer xmlSerializerAddComment = new XmlSerializer(typeof(RootXmlAddComment));
+            RootXmlAddComment? rootXmlAddComment = (RootXmlAddComment)xmlSerializerAddComment.Deserialize(strReaderAddComment);
+
+            long commentId = rootXmlAddComment.commentId;
+
+            //Get newly created card details and assert the comment is successfully created.
+            RootXmlGetTaskDetails? rootXmlGetTaskDetails = this.GetTaskDetails(boardId, newlyCreatedCardID);
+
+            List<CommentDetails> comments = rootXmlGetTaskDetails.commentItems.comments;
+
+            bool isCommentCreated = false;
+            foreach (CommentDetails comment in comments)
+            {
+                if (comment.commentId == commentId)
+                {
+                    Assert.AreEqual(newComment, comment.text);
+                    isCommentCreated = true;
+                }
+            }
+
+            if (!isCommentCreated)
+            {
+                Assert.Fail("Comment is not created properly!");
+            }
+        }
+
+        [Test]
+        public void Test_AddComment_InvalidData()
+        {
+            const string expectedErrMsg = "The taskid parameter was not set correctly.";
+            const string newComment = "This is a new test comment";
+
+            RestRequest requestAddComment = new RestRequest(apiUrl + "/add_comment/", Method.Post);
+            requestAddComment.AddHeader("apikey", apiKey);
+
+            RestResponse responseAddComment = this.client.Execute(requestAddComment);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, responseAddComment.StatusCode);
+
+            StringReader strReaderAddComment = new StringReader(responseAddComment.Content);
+
+            XmlSerializer xmlSerializerAddComment = new XmlSerializer(typeof(RootXmlInvalidCardInput));
+            RootXmlInvalidCardInput? rootXmlInvalidCardInput = (RootXmlInvalidCardInput)xmlSerializerAddComment.Deserialize(strReaderAddComment);
+
+            Assert.AreEqual(expectedErrMsg, rootXmlInvalidCardInput.errMsg);
+        }
+
+            private RootXmlAllCardsFromBoard GetAllTasksFromBoard(long boardId, params string[] columns)
         {
             if (columns.Length == 0)
             {
@@ -408,7 +736,7 @@ namespace Kanbanize.Api.Tests
         {
             RestRequest request = new RestRequest(apiUrl + "/get_task_details/", Method.Post);
             request.AddHeader("apikey", apiKey);
-            request.AddBody(new {boardid = boardId, taskid = createdCardId});
+            request.AddBody(new {boardid = boardId, taskid = createdCardId, comments = "yes"});
 
             RestResponse response = this.client.Execute(request);
 
